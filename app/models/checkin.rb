63c9853d92c08473a26d2e8b8d06ca1008ca1106
kayhide <kayhide@gmail.com>
2013-11-17 15:17:05 +0900

    update checkin#new.

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,4 +2,12 @@ class Checkin < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
   belongs_to :project
+
+  def project_github
+    self.project.try(:github)
+  end
+
+  def project_github= arg
+    self.project = Project.find_or_create_by(github: arg)
+  end
 end
