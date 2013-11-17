d36b43799a213b9a5a54ae805a34f468a91d57cc
takuya.miyamoto <takuya.miyamoto@mail.rakuten.com>
2013-11-17 17:27:09 +0900

    Add github client

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,15 @@
+require 'git_hub'
+
 class Project < ActiveRecord::Base
   has_many :checkins
   has_many :users, through: :checkins
+
+  def repository
+    GitHub.repository github
+  end
+
+  def readme
+    GitHub.readme github
+  end
 end
+
