38589206241a9a9e1565e55fe735238aea12ebf0
kayhide <kayhide@gmail.com>
2013-11-17 10:43:45 +0900

    init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reatech::Application.config.secret_key_base = '53e914e31596f8575ec909db3d7f0783f3f03e94f09b2fdde5c37558533257f1359615da10b91630b68bf8d7c5b49c07f8a333f8bcd7191ca8b876239ab98797'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reatech::Application.config.session_store :cookie_store, key: '_reatech_session'
